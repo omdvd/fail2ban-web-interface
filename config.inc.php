aba9c2e0da1d34cb2672bcc3a91be09d9964f8fa
Olexandr Davydenko <11025477+omdvd@users.noreply.github.com>
2025-05-12 15:21:35 +0300

    fix: add missing quotes to language code in settings

--- config.inc.php
+++ config.inc.php
@@ -8,7 +8,7 @@
 $f2b['usedns']=true; # show hostnames per banned IP [true|false]
 $f2b['noempt']=true; # do not show jails without banned clients [true|false]
 $f2b['jainfo']=true; # show jail information in table headers [true|false]
-$language=ptbr; #change language available options are English [en] or Brazilian Portuguese [ptbr]
+$language='ptbr'; #change language available options are English [en] or Brazilian Portuguese [ptbr]
 
 ######################
 # DO NOT EDIT PLEASE #
